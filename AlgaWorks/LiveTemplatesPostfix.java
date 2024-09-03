5084dde79e71ffb81eafde376e45bffbe6fe10ab
Levi Nicola <levinicolas2000@gmail.com>
2024-09-03 18:23:10 -0300

    add: LiveTemplatesPostfix.java

---
+++ AlgaWorks/LiveTemplatesPostfix.java
@@ -0,0 +1,30 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class LiveTemplatesPostfix {
+
+    public static void main(String[] args) {
+        int total = 10;
+        boolean pendente = true;
+
+        // Live template: souf
+        System.out.printf("");
+
+        // Postfix: total.fori
+        for (int i = 0; i < total; i++) {
+
+        }
+
+        // Postfix: total.forr
+        for (int i = total; i > 0; i--) {
+            
+        }
+
+        // Postfix: total.switch
+        switch (total) {
+
+        }
+
+        // Existem esses e vários outros atalhos uteis para a produtividade
+        // A visualização de todos está disponivel em: settings/editor/live templates
+    }
+}
