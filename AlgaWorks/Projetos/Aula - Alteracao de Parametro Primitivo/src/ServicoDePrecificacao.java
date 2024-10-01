b0026c29ffda8ba59706a457efc7384c5bd26a5d
Levi Nicola <levinicolas2000@gmail.com>
2024-09-30 21:18:30 -0300

    [add] pasta adicionada: Projetos/Aula - Alteracao de Parametro Primitivo

---
+++ AlgaWorks/Projetos/Aula - Alteracao de Parametro Primitivo/src/ServicoDePrecificacao.java
@@ -0,0 +1,6 @@
+public class ServicoDePrecificacao {
+    void calcularPrecoVenda(double precoCusto) {
+        // TODO implementar
+        precoCusto *= 1.20; // Adiciona 20% à variável
+    }
+}
--- AlgaWorks/Projetos/Aula - Alteracao de Variavel de Instancia/src/Main.java
+++ AlgaWorks/Projetos/Aula - Alteracao de Variavel de Instancia/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Aeronave aviaoGol = new Aeronave();
         aviaoGol.totalAssentos = 100;
-        
+
         aviaoGol.reservarAssentos(10);
 
         System.out.printf("GOL (%s): %d assentos disponiveis%n",
